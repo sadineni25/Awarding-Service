4cae1705bbaea8d1920cd467c73ffe6abecaf06f
Raghavendra Babu Sadineni <rbabu.sadineni@gmail.com>
2023-02-18 13:08:01 -0600

    Added spring-boot project base template for awarding-service

---
+++ src/main/java/com/award/points/AwardingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.award.points;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwardingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwardingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/award/points/AwardingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.award.points;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwardingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
